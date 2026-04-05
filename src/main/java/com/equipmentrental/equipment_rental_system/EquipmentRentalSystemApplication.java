728c684476ee1e57d85ab37b8305e5a27618be0b
Stanislav Starishko <slavkost73@gmail.com>
2026-04-05 17:02:07 +0100

    First commit

---
+++ src/main/java/com/equipmentrental/equipment_rental_system/EquipmentRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.equipmentrental.equipment_rental_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentRentalSystemApplication.class, args);
+	}
+
+}
