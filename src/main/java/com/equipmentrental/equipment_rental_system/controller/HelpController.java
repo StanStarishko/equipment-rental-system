fe6c3b9e21f760ffe4700143857db5eb0c665006
Stanislav Starishko <slavkost73@gmail.com>
2026-04-10 11:03:38 +0100

    Help page, contextual hints, FAQ, bug fixes

---
+++ src/main/java/com/equipmentrental/equipment_rental_system/controller/HelpController.java
@@ -0,0 +1,15 @@
+package com.equipmentrental.equipment_rental_system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelpController {
+
+    @GetMapping("/help")
+    public String help(Model model) {
+        model.addAttribute("activePage", "help");
+        return "help";
+    }
+}
